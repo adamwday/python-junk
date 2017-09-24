212ce90a24a33047366faae3ac36d336e1c1f7cf
adamwday <adam.day@outlook.com>
2017-09-23 23:25:01 -0600

    More fun with Python

--- first_program.py
+++ first_program.py
@@ -9,15 +9,26 @@ print 'hello world'
 print 'hello!!!!'
 
 #Addition
-2+2
+print 2+2
 #Subtraction
-2-2
+print 2-2
 #Multiplication
-2*2
+print 2*2
 #Division using floating point (for python 2.x only)
-2.0/2
+print 2.0/2
 #Division using float to give us 2.0/2 (for python 2.x only)
-float(2)/2
+print float(2)/2
 #Calculating two to the power of 3 (2^3)
-2**3
-#
+print 2**3
+#Adding an equation using an order of operations.
+print (2+10)*(10+3)
+#Adding a label to something using "="
+print a = 5
+#Using the variable "a"
+print a + a
+#Simple tax calculation using variables.
+my_income = 100
+tax_rate = 0.1
+my_taxes = my_income*tax_rate
+my_taxes
+print my_taxes
