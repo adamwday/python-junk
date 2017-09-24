0f6bdf659586d6c9df6792f365dfb9a5d6c5a8fa
adamwday <adam.day@outlook.com>
2017-09-23 23:16:16 -0600

    Adding some fun python code

---
+++ first_program.py
@@ -0,0 +1,23 @@
+########################################################################
+# This sample python code just so I can get a feel for coding in python.
+########################################################################
+
+#Importing division from python 3.0
+from __future__ import division
+
+print 'hello world'
+print 'hello!!!!'
+
+#Addition
+2+2
+#Subtraction
+2-2
+#Multiplication
+2*2
+#Division using floating point (for python 2.x only)
+2.0/2
+#Division using float to give us 2.0/2 (for python 2.x only)
+float(2)/2
+#Calculating two to the power of 3 (2^3)
+2**3
+#
